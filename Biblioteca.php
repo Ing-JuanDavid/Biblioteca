03c86169dc1911d7934402ad533a90c9bc677607
Luisda1101 <lvergarasolano62@correo.unicordoba.edu.co>
2025-03-21 19:55:50 -0500

    three commit

--- Biblioteca.php
+++ Biblioteca.php
@@ -1,4 +1,98 @@
 <?php
 class Biblioteca {
-    
+    public $libros = [];
+    public $usuarios = [];
+    public $prestamos = [];
+
+    // Constructor que carga los datos desde los archivos planos
+    public function __construct() {
+        $this->setData();
+    }
+
+    public function loadData() {
+        if (file_exists(ARCHIVO_LIBROS)) {
+            $lineas = file(ARCHIVO_LIBROS, FILE_IGNORE_NEW_LINES);
+            foreach ($lineas as $linea) {
+                $this->libros[] = Libro::fromLine($linea);
+            }
+        }
+
+        if(file_exists(ARCHIVO_USUARIOS)) {
+            $lineas = file(ARCHIVO_USUARIOS, FILE_IGNORE_NEW_LINES);
+            foreach ($lineas as $linea) {
+                $this->usuarios[] = Usuario::fromLine($linea);
+            }
+        }
+
+        if(file_exists(ARCHIVO_PRESTAMOS)) {
+            $lineas = file(ARCHIVO_PRESTAMOS, FILE_IGNORE_NEW_LINES);
+            foreach ($lineas as $linea) {
+                $this->prestamos[] = Prestamo::fromLine($linea);
+            }
+        }
+    }
+
+    public function saveData() {
+
+        $lineas = array_map(function($libro) {
+            return $libro->toLine();
+        }, $this->libros);
+        file_put_contents(ARCHIVO_LIBROS, implode("\n", $lineas));
+
+        $lineas = array_map(function($usuario) {
+            return $usuario->toLine();
+        }, $this->usuarios);
+        file_put_contents(ARCHIVO_USUARIOS, implode("\n", $lineas));
+
+        $lineas = array_map(function($prestamo) {
+            return $prestamo->toLine();
+        }, $this->prestamos);
+        file_put_contents(ARCHIVO_PRESTAMOS, implode("\n", $lineas));
+    }
+
+    public function searchBook($termino) {
+        $resultado = [];
+        foreach ($this->libros as $libro) {
+            if (
+                stripos($libro->titulo, $termino) !== false ||
+                stripos($libro->autor, $termino) !== false ||
+                stripos($libro->materia, $termino) !== false
+            ) {
+                $resultado[] = $libro;
+            }
+        }
+        return $resultado;
+    }
+
+    public function addBook($libro) {
+        $this->libros[] = $libro;
+        $this->saveData();
+    }
+
+    public function addUser($usuario) {
+        $this->usuarios[] = $usuario;
+        $this->saveData();
+    }
+
+    public function registerLoan($usuarioId, $libroId, $fecha) {
+        $prestamo = new Prestamo($usuarioId, $libroId, $fecha);
+        $this->prestamos[] = $prestamo;
+        $this->saveData();
+    }
+
+    public function getNameUser($usuarioId) {
+        foreach ($this->usuarios as $usuario) {
+            if ($usuario->id == $usuarioId) {
+                return $usuario->nombre;
+            }
+        }
+    }
+
+    public function getNameBook($libroId) {
+        foreach ($this->libros as $libro) {
+            if ($libro->id == $libroId) {
+                return $libro->titulo;
+            }
+        }
+    }
 }
--- config.php
+++ config.php
@@ -2,3 +2,7 @@
 require_once "Biblioteca.php";
 $Biblioteca = new Biblioteca();
 $miBiblioteca->cargarDatos();
+
+define('ARCHIVO_LIBROS', 'data/libros.txt');
+define('ARCHIVO_USUARIOS', 'data/usuarios.txt');
+define('ARCHIVO_PRESTAMOS', 'data/prestamos.txt');
